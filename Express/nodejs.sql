01309b17bf6d82efad2532e05022646023823e1c
cs865546153 <865546153@qq.com>
2018-04-10 10:37:57 +0800

    Update nodejs.sql

--- Express/nodejs.sql
+++ Express/nodejs.sql
@@ -33,7 +33,7 @@ CREATE TABLE `admin_role` (
   `role_name` varchar(15) NOT NULL DEFAULT '' COMMENT '角色名',
   `role_auths` text COMMENT '权限点',
   PRIMARY KEY (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='后台用户角色表';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 /*Data for the table `admin_role` */
 insert  into `admin_role`(`role_id`,`role_name`,`role_auths`) values (1,'管理员','user,role,sms'),(2,'运营','sms');
@@ -60,15 +60,18 @@ CREATE TABLE `items` (
   `item_price` int(10) DEFAULT NULL,
   `item_price_type` char(32) DEFAULT 'rmb/500g',
   `item_provider` char(32) NOT NULL,
+  `item_provider_region` char(32) DEFAULT 'SuZhou',
   PRIMARY KEY (`item_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
 /*Data for the table `items` */
-insert  into `items`(`item_id`,`item_name`,`item_price`,`item_provider`) values (1,'红烧排骨',22,'KFC'),(2,'椒盐排条',33,'KFC'),(3,'炒白菜',33,'MDL');
+insert  into `items`(`item_id`,`item_name`,`item_price`,`item_provider`) values (1,'青菜',22,'KFC'),(2,'猪肉',33,'KFC'),(3,'牛肉',33,'MDL');
 
 
 CREATE TABLE `providers` (
   `provider_id` int(10) NOT NULL AUTO_INCREMENT,
   `provider_name` varchar(10) NOT NULL,
+  `provider_region` char(32) DEFAULT 'SuZhou',
   PRIMARY KEY (`provider_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
